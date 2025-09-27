69b34490de8912526e76b4fbfaddb6aa9015b641
Ionela Lazer <ionela.lazer@yandex.ru>
2025-09-27 19:31:29 +0300

    PR18 ver.1

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,21 +5,41 @@ import 'dotenv/config'
 import express, { json, urlencoded } from 'express'
 import mongoose from 'mongoose'
 import path from 'path'
+import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 import { DB_ADDRESS } from './config'
 import errorHandler from './middlewares/error-handler'
-import serveStatic from './middlewares/serverStatic'
+// import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
 
 const { PORT = 3000 } = process.env
 const app = express()
 
+app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      limit: 100,
+      standardHeaders: true,
+      legacyHeaders: false,
+}));
+
 app.use(cookieParser())
 
-app.use(cors())
-// app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({ 
+    origin: process.env.ORIGIN_ALLOW || 'http://localhost:5173',
+    credentials: true 
+}));
 
-app.use(serveStatic(path.join(__dirname, 'public')))
+app.use('/images', (_req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+  next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(serveStatic(path.join(__dirname, 'public')))
 
 app.use(urlencoded({ extended: true }))
 app.use(json())
@@ -28,8 +48,7 @@ app.options('*', cors())
 app.use(routes)
 app.use(errors())
 app.use(errorHandler)
-
-// eslint-disable-next-line no-console
+app.use(helmet())
 
 const bootstrap = async () => {
     try {
