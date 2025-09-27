c3c4fa42146f137c260c80d83ff2fc2eefbbe3cc
Ionela Lazer <ionela.lazer@yandex.ru>
2025-09-28 00:54:22 +0300

    PR18. ver.5

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -27,7 +27,8 @@ app.use(cookieParser())
 
 app.use(cors({ 
     origin: true,
-    credentials: true,  // ← важно!
+    credentials: true,  
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
 }));
 
